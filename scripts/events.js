b0598c49a3037afdd3e55c7624cd9f8209feed1e
Steven Holmes <steven.w.holmes@gmail.com>
2016-10-27 15:35:31 -0500

    added events.js tag to HTML + first event listeners

---
+++ scripts/events.js
@@ -0,0 +1,13 @@
+// DOM VARIABLES
+
+var largeCheck = document.getElementById('largeCheck');
+
+// EVENT LISTENERS
+
+largeCheck.addEventListener('click', largeFunc);
+
+// FUNCTIONS
+
+function largeFunc() {
+	console.log('click');
+};
