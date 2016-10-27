a6e3d526c189b2a807a5a8325c8e41ae5cdb22c5
Steven Holmes <steven.w.holmes@gmail.com>
2016-10-27 15:40:25 -0500

    added updated event.js + other empty js files

--- scripts/events.js
+++ scripts/events.js
@@ -1,13 +1,27 @@
 // DOM VARIABLES
 
 var largeCheck = document.getElementById('largeCheck');
+var inputField = document.getElementById('inputField');
 
 // EVENT LISTENERS
 
 largeCheck.addEventListener('click', largeFunc);
+inputField.addEventListener('keyup', checkEnter);
 
 // FUNCTIONS
 
-function largeFunc() {
+function largeFunc() { // Toggles font-size to be larger when checked
 	console.log('click');
+};
+
+function checkEnter(keyup) { // Checks value of inputField on "Enter" press
+	if (keyup.keyCode === 13 && inputField.value.length >= 1) { // Only submits if inputField is has 1 or more characters
+		console.log('keyup')
+		clearInput();
+
+	};
+};
+
+function clearInput() {
+	console.log("clearInput");
 };
