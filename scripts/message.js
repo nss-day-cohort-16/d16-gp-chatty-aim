7674176368a2befc9c07690c06182fe0740dd83e
TrentHand <livefree1983@gmail.com>
2016-10-29 11:46:29 -0500

    completed loaded initial messages in DOM

--- scripts/message.js
+++ scripts/message.js
@@ -1,10 +1,6 @@
 var Chatty = (function (oldChatty) {
 	var currentMessages = [];
 
-	oldChatty.showMessages = currentMessages.forEach(function(item) {
-		outputBox.innerHTML += `<div id=${item.id}>${item.message}<button class="btn btn-danger" id="delete${item.id}">Delete</button></div>`;
-		});
-
 	var addNewMessage = function(userInput) {
 		oldChatty.addMessage(userInput);
 // Pulling new message from "pull.js"
--- scripts/pull.js
+++ scripts/pull.js
@@ -1,22 +1,41 @@
 var Chatty = (function(oldChatty) {
      var _messages = [];
 
-     oldChatty.loadMessages = function() {
+     oldChatty.loadMessages = function(callbackFunction) {
           var loader = new XMLHttpRequest();
           loader.open("GET", "messages.JSON");
           loader.send();
-          loader.addEventListener("load", parseData);
-          loader.addEventListener("error", failAlert);
-     };
-
-     function parseData() {
+          loader.addEventListener("load", function() {
           _messages = JSON.parse(this.responseText).messages;
           // console.log("_messages", _messages);
-     }
-
-     function failAlert() {
+          callbackFunction(_messages);
+          Chatty.loadMessages(Chatty.listMessages);
+          });
+          loader.addEventListener("error", function() {
           console.log("The data didn't come!");
-     }
+          });
+     };
+
+
+
+     oldChatty.listMessages = function (messages) {
+     //List messages in DOM
+          var outputBox = document.getElementById("outputBox");
+          var outputMessage = "";
+
+          for (var i = 0; i < messages.length; i++) {
+               var currentMessage = messages[i];
+
+               outputMessage += `<div class="alert alert-danger">
+                         ${currentMessage.message} <button type="button" class="btn btn-success" id="${currentMessage.id}">Delete</button></div>`;
+          }
+
+          outputBox.innerHTML = outputMessage;
+     };
+
+
+
+
 
      oldChatty.getMessages = function() {
           // console.log(oldChatty.getMessages);
@@ -28,4 +47,3 @@ var Chatty = (function(oldChatty) {
 
 })(Chatty || {});
 
-// console.log("chatty", Chatty);
